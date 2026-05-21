576777abe0923bfe5339947101a2586c58ee10c6
ArpitaNaik18 <126313348+ArpitaNaik18@users.noreply.github.com>
2026-05-21 10:02:45 +0530

    topmodule.v

--- src/design/top.v
+++ src/design/top.v
@@ -1,22 +1,50 @@
 `timescale 1ns / 1ns
-module uart #(parameter clk_freq=50000000,baud_rate=2400,width=8)(
-input sys_clk,
-input sys_rst,
-input xmit_h,
-input [width-1:0] xmit_data_h,
-input uart_rec_data_h,
-output baud_op_clk,
-output uart_xmit_data_h,
-output xmit_done_h,
-output [width-1:0] rec_data_h,
-output rec_ready,
-output rec_busy,
-output xmit_active
+
+module uart#(parameter N = 8)(
+    input              sys_clk,
+    input              sys_rst,
+    input              xmith,
+    input  [N-1:0]     xmit_datah,
+    input              uart_rec_datah,
+    output             uart_xmit_datah,
+    output             xmit_doneh,
+    output             rec_readyh,
+    output [N-1:0]     rec_datah,
+    output             rec_busy,
+    output             xmit_active
 );
+    wire baud_clk;
 
+    baud#(
+        .BAUD(9600),
+        .FACTOR(16),
+        .XTAL(100_000_000)
+    ) u_baud (
+        .sys_clk (sys_clk),
+        .rst(sys_rst),
+        .baud_clk(baud_clk)
+    );
 
+    uart_tx#(.N(N)) u_tx (
+        .sys_clk(sys_clk),
+        .rst(sys_rst),
+        .baud_clk(baud_clk),
+        .xmith (xmith),
+        .xmit_datah (xmit_datah),
+        .xmit_doneh (xmit_doneh),
+        .xmit_active(xmit_active),
+        .uart_xmit_dataH(uart_xmit_datah)
+    );
 
-baud #(.clk_freq(clk_freq),.baud_rate(baud_rate)) b1 (.sys_clk(sys_clk),.sys_rst(sys_rst),.baud_op_clk(baud_op_clk));
-uart_tx #(.width(width)) b2(.baud_op_clk(baud_op_clk),.sys_rst(sys_rst),.xmit_h(xmit_h),.xmit_data_h(xmit_data_h),.xmit_active(xmit_active),.xmit_done_h(xmit_done_h),.uart_xmit_data_h(uart_xmit_data_h));
-uart_rx #(.width(width)) b3(.baud_op_clk(baud_op_clk),.sys_rst(sys_rst),.uart_rec_data_h(uart_xmit_data_h),.rec_ready(rec_ready),.rec_busy(rec_busy),.rec_data_h(rec_data_h));
+    uart_rx#(.N(N)) u_rx (
+        .sys_clk(sys_clk),
+        .rst(sys_rst),
+        .baud_clk (baud_clk),
+        .xmith(xmith),
+        .uart_rec_datah(uart_rec_datah),
+        .rec_dataH(rec_datah),
+        .rec_readyH(rec_readyh),
+        .rec_busy(rec_busy)
+    );
 endmodule
+
