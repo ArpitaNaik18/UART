4dbdac3aedf5001609fff873ab0920d718984512
ArpitaNaik18 <126313348+ArpitaNaik18@users.noreply.github.com>
2026-05-21 09:57:13 +0530

    baudclk.v

--- src/design/baudclk.v
+++ src/design/baudclk.v
@@ -1,30 +1,28 @@
-module baud #(parameter clk_freq=5000000,baud_rate=2400)(
-	input sys_clk,
-	input sys_rst,
-	output reg baud_op_clk);
-	
-	integer count;
-	
-	parameter count1= (clk_freq)/(16*baud_rate);
-	
-	always @(posedge sys_clk or negedge sys_rst)
-     	begin
-			if(!sys_rst) begin
-			baud_op_clk<=0;
-			count<=0;
-			end
-			
-			else if (count==(count1-1))
-			begin
-			baud_op_clk<=~baud_op_clk;
-			count<=0;
-			end
-			
-			else
-			begin
-			baud_op_clk<=0;
-			count<=count+1;
-			end
-			
-		end
+module baud #(
+    parameter BAUD=9600,
+    parameter FACTOR=16,
+    parameter XTAL=100_000_000
+)(
+    input sys_clk,
+    input rst,         
+    output reg baud_clk
+);
+   
+    localparam integer CLK_DIV=XTAL / (FACTOR*BAUD*2);
+    localparam integer CW=$clog2(CLK_DIV);
+    reg [CW-1:0] count;
+    always @(posedge sys_clk or negedge rst) begin
+        if (!rst) begin
+            baud_clk <= 1'b0;
+            count<= 0;
+        end
+        else begin
+            if (count == CLK_DIV - 1) begin
+                count<=0;
+                baud_clk<=~baud_clk;
+            end
+            else
+                count<=count + 1;
+        end
+    end
 endmodule
